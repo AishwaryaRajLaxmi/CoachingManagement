e8834d4846301693eed88e216d44413393f67cee
AishwaryaRajLaxmi <111491285+AishwaryaRajLaxmi@users.noreply.github.com>
2024-06-27 20:24:24 +0530

    Update Login.java

--- src/management/student/Login.java
+++ src/management/student/Login.java
@@ -12,10 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author Tanu Singh
- */
+
 public class Login extends javax.swing.JFrame {
 
     /**
@@ -193,11 +190,7 @@ public class Login extends javax.swing.JFrame {
      * @param args the command line arguments
      */
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+        
         try {
             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
                 if ("Nimbus".equals(info.getName())) {
